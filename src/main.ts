247b46c56206e1996cde0a2c68ecf3562f700b2a
samuel <samuelcuervo1224@gmail.com>
2025-11-28 10:04:23 -0500

    comit despliegue

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,12 @@ async function bootstrap() {
     }),
   );
 
+  app.enableCors({
+    origin: ['http://localhost:8081'], 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 4000);
 }
 
